2c996963fea33a3ea5eec3317ea265823f69c6fc
刘旺 <liuwang@kuaishou.com>
2024-08-06 23:11:48 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import WebApp from '@twa-dev/sdk';
 import './App.css'
 import YouTubePlayer from './pages/vedio/YoutubePlayer'
 
 function App() {
-  async function shareVedio() {
-    // 获取分享 url
-    const currVedioId = localStorage.getItem('currVedio')
-    const url = "https://youtu.be/" + currVedioId;
-    try {
-      WebApp.shareToStory(
-        url, 
-        {
-          text: '分享给别人'
-        }
-      );
-      console.log(`分享给别人 URL: ${url}`);
-    } catch (error) {
-      console.error('Error sharing URL:', error);
-    }
-  }
+  // async function shareVedio() {
+  //   // 获取分享 url
+  //   const currVedioId = localStorage.getItem('currVedio')
+  //   const url = "https://youtu.be/" + currVedioId;
+  //   try {
+  //     WebApp.shareToStory(
+  //       url, 
+  //       {
+  //         text: '分享给别人'
+  //       }
+  //     );
+  //     console.log(`分享给别人 URL: ${url}`);
+  //   } catch (error) {
+  //     console.error('Error sharing URL:', error);
+  //   }
+  // }
 
   return (
     <>
